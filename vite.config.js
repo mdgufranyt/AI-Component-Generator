3b279ece9a17656b3114f133b57c927f1735cd8b
mdgufranyt <gufranbareilly@gmail.com>
2026-02-22 19:34:06 +0530

    Fix Errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: [
+      "react",
+      "react-dom",
+      "react-dom/client",
+      "react-router-dom",
+      "@google/genai",
+      "@monaco-editor/react",
+      "react-icons",
+      "react-select",
+      "react-spinners",
+      "react-toastify",
+    ],
+  },
+  server: {
+    warmup: {
+      clientFiles: ["./src/main.jsx", "./src/App.jsx"],
+    },
+  },
+});
